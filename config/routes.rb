f799e48cfeebc10c4ff98867fd50d2822fcd7aa8
関口達也 <nakayubi03@yahoo.co.jp>
2019-11-10 10:19:00 +0900

    show-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/' => 'posts#index'
-  get 'posts/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/'           => 'posts#index'
+  get 'posts/index' => 'posts#index'
+  get 'posts/show'  => 'posts#show'
 end
