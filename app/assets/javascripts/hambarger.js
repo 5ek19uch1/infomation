0481fdc15780535e2e81a38cbb9b0a0a9c8dd4f9
関口達也 <nakayubi03@yahoo.co.jp>
2019-11-12 22:50:39 +0900

    modal動いた

--- app/assets/javascripts/hambarger.js
+++ app/assets/javascripts/hambarger.js
@@ -1,8 +1,23 @@
 $(function() {
-  $('.fa-bars').click(function() {
-    $('.ham-menu').fadeIn();
+  $('.menu-icon').click(function() {
+    $('#ham-menu').fadeIn();
   });
-  $('.close-modal').click(function(){
+  $('.fa-times').click(function(){
     $('.ham-menu').fadeOut();
   });
 });
+
+
+// $(function() {
+//   $(document).on('click','.fa-bars',function() {
+//     $('.ham-menu').fadeIn();
+//   });
+// });
+
+
+// $(document).ready(function(){
+//   $('fa-bars').click(function(){
+//     $('.ham-menu').fadeIn();
+//   });
+// });
+
