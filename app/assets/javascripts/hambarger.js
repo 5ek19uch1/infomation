77474627ee1c2bc25969efa4ca8dcbd249749110
関口達也 <nakayubi03@yahoo.co.jp>
2019-11-13 13:04:10 +0900

    edit : return_to_top

--- app/assets/javascripts/hambarger.js
+++ app/assets/javascripts/hambarger.js
@@ -8,16 +8,20 @@ $(function() {
 });
 
 
-// $(function() {
-//   $(document).on('click','.fa-bars',function() {
-//     $('.ham-menu').fadeIn();
-//   });
-// });
-
-
-// $(document).ready(function(){
-//   $('fa-bars').click(function(){
-//     $('.ham-menu').fadeIn();
-//   });
-// });
-
+jQuery(function() {
+  var pagetop = $('#page_top');
+  pagetop.hide();
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {  //100pxスクロールしたら表示
+          pagetop.fadeIn();
+      } else {
+          pagetop.fadeOut();
+      }
+  });
+  pagetop.click(function () {
+      $('body,html').animate({
+          scrollTop: 0
+      }, 500); //0.5秒かけてトップへ移動
+      return false;
+  });
+});
