d8a1fe6571994e1f92fc8b71535e2ceaa68dc877
関口達也 <nakayubi03@yahoo.co.jp>
2019-11-14 14:51:22 +0900

    new.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'posts#index'
-  get 'posts/index' => 'posts#index'
+  get 'index'       => 'posts#index'
   get 'posts/show'  => 'posts#show'
+  get 'posts/new'   => 'posts#new'
 end
