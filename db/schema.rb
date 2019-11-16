a9dec1ec2a506322690317968f092c46836d5bcc
関口達也 <nakayubi03@yahoo.co.jp>
2019-11-16 21:03:03 +0900

    2nd rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_16_114535) do
+ActiveRecord::Schema.define(version: 2019_11_16_115850) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "main_title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_11_16_114535) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
